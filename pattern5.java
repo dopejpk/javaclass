e38ec16ca6b4744c1ec86b29e5028aa4f71138ba
Prishika <prishikasridevi@gmail.com>
2026-09-03 20:01:07 +0530

    First Commit

---
+++ pattern5.java
@@ -0,0 +1,25 @@
+package patterns;
+
+public class pattern5 {
+
+	public static void main(String[] args) {
+			for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				int a =i+j;
+				if(a%2==0) {
+				System.out.print("1 ");
+			
+			}
+				else {
+			System.out.print("0 ");
+		}
+				
+}
+			System.out.println(" ");
+}
+
+	}
+
+	}
+
+
